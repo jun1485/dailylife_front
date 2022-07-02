c8ea280e83ad1493ec5c584c85bffd216c0a2f10
jun <wnwjdwns1@naver.com>
2022-07-02 20:26:30 +0900

    Create

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./components/card/card";
+import Cards from "./components/card/card";
+
+function App() {
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
