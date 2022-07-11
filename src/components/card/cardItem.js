795155473814a4a105ed6f60abcf0033014cf5d2
mimmel <mfblade2014@gmail.com>
2022-07-11 15:11:35 +0900

    feat: 좋아요 기능 carditem.js에 추가

--- src/components/card/cardItem.js
+++ src/components/card/cardItem.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux/es/exports";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import { postActions } from "../store/selectedPostData";
 
 function CardItem(props) {
+  
   const dispatch = useDispatch();
+
   const postData = useSelector((state) => state.selectedPostData);
 
   const openModal = (to) => {
     window.location.href = "/#open-modal";
   };
+
+  // like start
+
+  const [like, setLike] = useState(0);
+
+  const Fullheart = "/assets/full.png";
+
+  const Emptyheart = "/assets/empty.png";
+
+  const HandleLike = (event)=>{
+    event.stopPropagation();
+    if (like === 0){
+      setLike(1)
+    } else {
+      setLike(0)
+    }
+  }
+
+  // like end
+
   return (
     <>
       <li
@@ -30,6 +52,8 @@ function CardItem(props) {
         </figure>
         <div className="cards__item__info">
           <h5 className="cards__item__text">{props.text}</h5>
+          {/* like */}
+          <span onClick={HandleLike}><img className="cards__item__like" src= {like? Fullheart:Emptyheart}/></span>
           <p className="cards__item__underInfo">{props.underInfo}</p>
         </div>
       </li>
