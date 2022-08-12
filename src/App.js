3982a6d92ef81ee905f9a3c209869812603a10f4
NEARworld <artemismars2@outlook.com>
2022-08-12 15:21:38 +0900

    Modify: SignIn to SignUp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Cards from "./components/card/card";
 import Login from "./components/login/Login";
+import SignUp from "./components/login/SignUp";
 import { Route, Routes } from "react-router-dom";
-import SignIn from "./components/login/SignIn";
 import Searching from "./components/search/Searching";
 import RecentlyViewed from "./components/recentlyViewed/recentlyViewed";
 import UserPost from "./components/UserPost/UserPost";
@@ -45,7 +45,7 @@ function App() {
         }
       ></Route>
       <Route path="/login" element={<Login />}></Route>
-      <Route path="/signIn" element={<SignIn />}></Route>
+      <Route path="/signUp" element={<SignUp />}></Route>
       <Route path="/recentlyViewed" element={<RecentlyViewed />}></Route>
       <Route path="/search" element={<Searching cardData={cardData} />}></Route>
       <Route path="/post" element={<UserPost />} />
