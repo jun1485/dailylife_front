cd50febaa48f0daac8d3a68ea13b0722267fb422
NEARworld <artemismars2@outlook.com>
2022-08-15 17:49:56 +0900

    Fix: merge conflict해결

--- src/components/postModal/PostModal.js
+++ src/components/postModal/PostModal.js
@@ -1,6 +1,6 @@
-import "./PostModal.css";
 import { useSelector } from "react-redux/es/exports";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import styled from "styled-components";
 
 const tempComment = [
   {
@@ -28,9 +28,227 @@ const tempComment = [
   },
 ];
 
+const ModalWindow = styled.div`
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.25);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 999;
+  visibility: visible;
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 23px;
+
+  & > div {
+    border-radius: 1rem;
+  }
+  &:target {
+    visibility: visible;
+    opacity: 1;
+    pointer-events: auto;
+  }
+`;
+const ModalBody = styled.div`
+  position: absolute;
+  padding: 2em;
+  display: flex;
+  width: 1183px;
+  height: 650px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  visibility: visible;
+`;
+const ModalImageContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  background-color: pink;
+  border-radius: 10px;
+  background-position: center;
+  background-size: cover;
+  background-image: url(https://velog.velcdn.com/images/nearworld/post/f7eaaabf-8e80-4326-a85a-c8789c8ec68a/image.png);
+`;
+const ModalContentContainer = styled.div`
+  position: relative;
+  width: 50%;
+  margin-left: 20px;
+
+  & > .comment-create {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+
+    display: flex;
+  }
+`;
+const WriterInfoContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const WriterInfo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr;
+  gap: 10px;
+  justify-content: space-around;
+  align-items: center;
+`;
+const Avatar = styled.div`
+  border-radius: 50%;
+  width: 38px;
+  aspect-ratio: 1;
+  background-color: purple;
+`;
+const Follow = styled.button`
+  width: 68px;
+  height: 28px;
+  border: none;
+  border-radius: 100px;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 23px;
+`;
+const Username = styled.div``;
+const ModalCloseContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const ModalClose = styled.a`
+  position: relative;
+  margin-left: auto;
+  width: 18px;
+  aspect-ratio: 1;
+  color: #aaa;
+  text-decoration: none;
+
+  & svg {
+    position: absolute;
+  }
+  & svg:nth-of-type(1) {
+    transform: rotate(90deg);
+  }
+  &:hover {
+    color: black;
+  }
+`;
+const ModalContent = styled.div`
+  text-align: start;
+  margin-top: 12px;
+
+  & > .title-in-modal {
+    text-align: start;
+    font-size: xx-large;
+  }
+  & > .text-in-modal {
+    margin-bottom: 25px;
+  }
+  & hr {
+    background-color: #eaeaea;
+    height: 1px;
+    border: 0;
+  }
+`;
+const ModalSocial = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+`;
+const SocialIconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > .comments-icon {
+    margin-left: 10px;
+  }
+`;
+const SocialCountContainer = styled.div`
+  & > .comments-count {
+    margin-left: 10px;
+  }
+`;
+const CommentSection = styled.div`
+  height: 260px;
+  overflow-y: auto;
+
+  & > .comment-container {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+  }
+  & > .comment-container > .comment-main {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
+const CommentContainer = styled.div.attrs({
+  className: "comment-container",
+})`
+  & > .comment-date-container {
+    display: flex;
+  }
+`;
+const CommentMain = styled.div.attrs({ className: "comment-main" })`
+  & > .username {
+    margin-left: 10px;
+    font-weight: 600;
+  }
+  & > .comment-content {
+    flex: 3;
+    margin-left: 10px;
+  }
+  & > .comment-like > svg {
+    width: 14.08px;
+    height: 12.94px;
+  }
+`;
+const CommentDateContainer = styled.div.attrs({
+  className: ".comment-date-container",
+})`
+  & > .empty {
+    width: 38px;
+    aspect-ratio: 1;
+  }
+  & > .comment-date {
+    margin-left: 10px;
+    font-size: 13px;
+    color: #848484;
+  }
+  & > .comment-date > .comment-expand {
+    font-weight: 500;
+  }
+  & > .comment-sub-list {
+    display: flex;
+  }
+`;
+const CommentCreate = styled.div.attrs({ className: "comment-create" })`
+  & > .comment-create-text {
+    border: 1px solid #dcdcdc;
+    border-radius: 100px;
+    width: 100%;
+    margin-left: 10px;
+    padding: 10px;
+    font-family: "Pretendard";
+    font-size: 16px;
+  }
+`;
 function PostModal(props) {
   const currentPostData = useSelector((state) => state.selectedPostData);
-  console.log(currentPostData);
 
   useEffect(() => {
     if (localStorage.watched === undefined) {
@@ -47,43 +265,22 @@ function PostModal(props) {
   return (
     <>
       <div className="container">
-        <div
+        <ModalWindow
           id="open-modal"
-          onClick={(e) => {
-            window.location.href = "#home";
-          }}
-          className="modal-window"
+          onClick={(e) => (window.location.href = "#home")}
         >
-          <div
-            className="modal-inside"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
+          <ModalBody onClick={(e) => e.stopPropagation()}>
             {/* 이미지는 Carousel로 교체 예정 */}
-            <div
-              className="modal-src-container"
-              // style={{
-              //   backgroundImage: currentPostData.src,
-              // }}
-            >
-              <div className="modal-img">
-                <img
-                  className="modal-inside-img"
-                  src={currentPostData.src}
-                  alt="modal-img"
-                />
-              </div>
-            </div>
-            <div className="modal-content-container">
-              <div className="writer-info-container">
-                <div className="writer-info">
-                  <div className="avatar"></div>
-                  <div className="username">작성자닉네임</div>
-                  <button className="follow">팔로우</button>
-                </div>
-                <div className="modal-close-container">
-                  <a href="#home" title="Close" className="modal-close">
+            <ModalImageContainer />
+            <ModalContentContainer>
+              <WriterInfoContainer>
+                <WriterInfo>
+                  <Avatar />
+                  <Username>작성자닉네임</Username>
+                  <Follow>팔로우</Follow>
+                </WriterInfo>
+                <ModalCloseContainer>
+                  <ModalClose href="#home" title="title">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="18"
@@ -114,10 +311,10 @@ function PostModal(props) {
                         strokeLinejoin="round"
                       />
                     </svg>
-                  </a>
-                </div>
-              </div>
-              <div className="content-in-modal">
+                  </ModalClose>
+                </ModalCloseContainer>
+              </WriterInfoContainer>
+              <ModalContent>
                 <h1 className="title-in-modal">{currentPostData.title}</h1>
                 <div className="text-in-modal">
                   {currentPostData.content
@@ -127,10 +324,10 @@ function PostModal(props) {
                       ))}
                 </div>
                 {/* <div className="text-in-modal">{currentPostData.content}</div> */}
-              </div>
+              </ModalContent>
               {/* 좋아요 댓글 갯수 출력하는 코드 */}
-              <div className="social-in-modal">
-                <div className="social-icons-container">
+              <ModalSocial>
+                <SocialIconsContainer>
                   <span className="likes-icon">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -164,19 +361,19 @@ function PostModal(props) {
                       />
                     </svg>
                   </span>
-                </div>
-                <div className="social-count-container">
+                </SocialIconsContainer>
+                <SocialCountContainer>
                   <span className="likes-count">좋아요 {10}개</span>
                   <span className="comments-count">댓글 {3}개</span>
-                </div>
-              </div>
+                </SocialCountContainer>
+              </ModalSocial>
               <hr />
               {/* 댓글 창 시작 */}
-              <div className="comments-section">
+              <CommentSection>
                 {tempComment.map((item, index) => (
-                  <div key={index} className="comment-container">
-                    <div className="comment-main">
-                      <div className="avatar"></div>
+                  <CommentContainer>
+                    <CommentMain>
+                      <Avatar />
                       <div className="username">홍길동</div>
                       <div className="comment-content">댓글입니다!</div>
                       <span className="comment-like">
@@ -196,8 +393,8 @@ function PostModal(props) {
                           />
                         </svg>
                       </span>
-                    </div>
-                    <div className="comment-date-container">
+                    </CommentMain>
+                    <CommentDateContainer>
                       <span className="empty"></span>
                       <span className="comment-date">
                         {1}일 ·{" "}
@@ -222,22 +419,22 @@ function PostModal(props) {
                           <div className="avatar"></div>
                         </li>
                       </ul> */}
-                    </div>
-                  </div>
+                    </CommentDateContainer>
+                  </CommentContainer>
                 ))}
-              </div>
+              </CommentSection>
               {/* 댓글 작성칸 */}
-              <div className="comment-create">
-                <div className="avatar"></div>
+              <CommentCreate>
+                <Avatar />
                 <input
                   type="text"
                   className="comment-create-text"
                   placeholder="댓글 달기"
                 ></input>
-              </div>
-            </div>
-          </div>
-        </div>
+              </CommentCreate>
+            </ModalContentContainer>
+          </ModalBody>
+        </ModalWindow>
       </div>
     </>
   );
