8812495c1d828f893c5e614e70ddeb405f80aeaf
NEARworld <artemismars2@outlook.com>
2022-08-18 14:13:10 +0900

    Modify: CardItem 코드 유지보수
    `onClick`에 있던 `openModal()`과 `dispatch()`를 `handleClick`함수로 이동시켜
    `jsx` 영역에는 최대한 html태그만 있도록 했습니다.

--- src/components/card/cardItem.js
+++ src/components/card/cardItem.js
@@ -1,6 +1,4 @@
-import axios from "axios";
 import React, { useState } from "react";
-import { useSelector } from "react-redux/es/exports";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import styled from "styled-components";
 import { selectedPostActions } from "../store/selectedPostData";
@@ -9,11 +7,19 @@ const CardItemText = styled.p``;
 
 function CardItem(props) {
   const dispatch = useDispatch();
-  const store = useSelector((state) => state);
-
   const openModal = (to) => {
     window.location.href = "/#open-modal";
   };
+  function handleClick() {
+    openModal();
+    dispatch(
+      selectedPostActions.updateData({
+        src: props.src,
+        title: props.title,
+        content: props.content,
+      })
+    );
+  }
 
   // like start
 
@@ -35,19 +41,7 @@ function CardItem(props) {
 
   return (
     <>
-      <li
-        className="cards__item"
-        onClick={() => {
-          openModal();
-          dispatch(
-            selectedPostActions.updateData({
-              src: props.src,
-              title: props.title,
-              content: props.content,
-            })
-          );
-        }}
-      >
+      <li className="cards__item" onClick={handleClick}>
         <figure className="cards__item__pic-wrap">
           <img className="cards__item__img" alt="img" src={props.src} />
           <div className="cards__item__gradientBar" />
@@ -59,6 +53,7 @@ function CardItem(props) {
             <img
               className="cards__item__like"
               src={like ? Fullheart : Emptyheart}
+              alt="like"
             />
           </span>
           <p className="cards__item__underInfo">{props.content}</p>
