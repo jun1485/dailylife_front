7f553c6e7809451d07e74bc81b9ce53d6556db99
Simin-Kim <show3391@gmail.com>
2022-08-22 14:25:33 +0900

    Feat: userNum으로 내 정보 페이지에 접근 기능 추가

--- src/components/myInfo/MyInfo.js
+++ src/components/myInfo/MyInfo.js
@@ -1,8 +1,29 @@
+import { type } from "@testing-library/user-event/dist/type";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { myInfoActions } from "../store/myInfo"
+
 export default function MyInfo(){
+    const userData = useSelector(state=>state.myInfo)
+    const dispatch = useDispatch()
+    useEffect(()=>{
+          if (!userData.userInfo){
+            axios
+              .post(`${process.env.REACT_APP_HOST}/api/users/details/${userData.userNum}` ,null, {
+                headers:{
+                    "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
+                }
+              })
+              .then(res=>dispatch(myInfoActions.updateUserInfo(res.data.data)))
+              .catch(err=>console.log(err))
+          }
+    },[])
+
 
     return(
-        <div style={ { textAlign:'center', marginTop:'60px'} }> 
-            내 정보 페이지입니다.
+        <div style={ { textAlign:'center', marginTop:'60px'} }>
+            {userData.userInfo.userName}님의 회원 정보 페이지입니다.
         </div>
     )
 }
--- src/components/store/myInfo.js
+++ src/components/store/myInfo.js
@@ -4,7 +4,19 @@ const myInfo = createSlice({
     name: "myInfo",
     initialState: {
         userNum: "",
+        userInfo: "",
     },
+
+    reducers: {
+        updateUserNum(state,aciton){
+            state.userNum = aciton.payload
+        },
+        updateUserInfo(state, action){
+            state.userInfo = action.payload
+        },
+    }
 });
+
+export const myInfoActions = myInfo.actions
 
 export default myInfo;
