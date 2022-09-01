6c42128bc94eef1c457c3fa029424eafe5bf3136
NEARworld <artemismars2@outlook.com>
2022-09-01 16:11:41 +0900

    refactor: login에서 아이디,비밀번호 찾기 & 회원가입 분리

---
+++ src/components/login/LoginForm/LoginFind/index.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import LoginThird from '../LoginThird';
+
+function LoginFind() {
+  return (
+    <LoginFindContainer>
+      <FindUser>
+        <Link to="/#">아이디 · 비밀번호 찾기</Link>
+      </FindUser>
+      <SignUp>
+        <Link to="/signup">회원가입</Link>
+      </SignUp>
+      <Separate />
+      <LoginThird />
+    </LoginFindContainer>
+  );
+}
+
+export default LoginFind;
+const LoginFindContainer = styled.div`
+  margin-top: 16px;
+  text-align: start;
+`;
+const FindUser = styled.span`
+  font-weight: 400;
+  font-size: 16px;
+
+  color: #6a6a6a;
+`;
+const SignUp = styled.span`
+  float: right;
+  font-weight: 600;
+  font-size: 16px;
+  color: #6a6a6a;
+`;
+const Separate = styled.div`
+  margin-top: 50px;
+  border: 1px #d7d7d7 solid;
+`;
