aee2125703088b86a5d9b754946e0319dabff6dd
NEARworld <artemismars2@outlook.com>
2022-09-01 16:15:21 +0900

    refactor: login 분리 세분화 후 Login.js를 index.jsx로 변경

---
+++ src/components/login/index.jsx
@@ -0,0 +1,12 @@
+import './login.css';
+import LoginForm from './LoginForm';
+
+function Login() {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
