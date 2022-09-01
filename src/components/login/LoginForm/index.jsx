a0530a95fa6198a9fe0eab09bce03c8a02d9a1c3
NEARworld <artemismars2@outlook.com>
2022-09-01 16:13:39 +0900

    refactor: login에서 로그인 form 분리

---
+++ src/components/login/LoginForm/index.jsx
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { SET_TOKEN } from '../../../reducers/authToken';
+import { myInfoActions } from '../../../reducers/myInfo';
+import { LoadingSpinner } from '../../styledComponents/Loading';
+import LoginBtn from './LoginBtn/index';
+import LoginFind from './LoginFind/index';
+import LoginInput from './LoginInput/index';
+
+function LoginForm() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const tokenInfo = useSelector((state) => state.authToken);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+
+    axios
+      .post(
+        `${process.env.REACT_APP_HOST}/api/users/login`,
+        {
+          userId: username,
+          userPassword: password,
+        },
+        {
+          headers: {
+            'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+          },
+        },
+      )
+      .then((res) => {
+        setLoading(false);
+        localStorage.setItem('accessToken', res.data.data.accessToken);
+        console.log(res);
+        dispatch(SET_TOKEN(res.data.data.accessToken));
+        console.log('token: ' + JSON.stringify(tokenInfo));
+        dispatch(myInfoActions.updateUserNum(res.data.data.userNum));
+        navigate('/');
+      })
+      .catch((errStatus) => {
+        setLoading(false);
+        console.log(errStatus); // eslint-disable-line no-console
+        if (errStatus.response.status === 400) {
+          alert('아이디와 비밀번호를 입력해주세요 !');
+        } else if (errStatus.response.status === 500) {
+          alert('아이디 또는 비밀번호를 잘못 입력했습니다. \n입력하신 내용을 다시 확인해주세요.');
+        }
+      });
+  }
+
+  return (
+    <FormContainer>
+      <form action="/login" method="POST" onSubmit={handleSubmit}>
+        {loading && <LoadingSpinner />}
+        <LoginInput username={username} setUsername={setUsername} password={password} setPassword={setPassword} />
+        <LoginBtn />
+        <LoginFind />
+        {/* <p className="message">
+            {" "}
+            Not registered? <Link to="/SignIn">Create an account</Link>
+          </p> */}
+      </form>
+    </FormContainer>
+  );
+}
+
+export default LoginForm;
+const FormContainer = styled.form`
+  max-width: 320px;
+  margin-top: 200px;
+  text-align: center;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  line-height: 19px;
+`;
