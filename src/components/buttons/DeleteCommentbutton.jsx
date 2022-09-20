f71b89cccf617df6bcc3b7f0984b94d01a29c8d9
NEARworld <artemismars2@outlook.com>
2022-09-20 15:09:58 +0900

    chore: DeleteCommentbutton 들여쓰기 수정

--- src/components/buttons/DeleteCommentbutton.jsx
+++ src/components/buttons/DeleteCommentbutton.jsx
@@ -1,65 +1,53 @@
 import axios from 'axios';
 
 function DeleteCommentButton(props) {
-    const {
-        replyList,
-        setReplyList,
-        setReplyDeleteFlag,
-        replyDeleteFlag,
-    } = props;
-    const replyDeleteHandler = (replyNum) => {
-        axios
-            .delete(
-                `${process.env.REACT_APP_HOST}/api/reply/delete/${replyNum}`,
-                {
-                    headers: {
-                        'X-ACCESS-TOKEN':
-                            localStorage.getItem('accessToken'),
-                    },
-                },
-            )
-            .then(() => {
-                const idx = replyList.findIndex(
-                    (item) => item.replyNum === replyNum,
-                );
-                const newReplyList = [...replyList];
-                newReplyList.splice(idx, 1);
-                setReplyList(newReplyList);
-                setReplyDeleteFlag(-1);
-            })
-            .catch((err) => console.log(err));
-    };
+  const { replyList, setReplyList, setReplyDeleteFlag, replyDeleteFlag } =
+    props;
+  const replyDeleteHandler = (replyNum) => {
+    axios
+      .delete(`${process.env.REACT_APP_HOST}/api/reply/delete/${replyNum}`, {
+        headers: {
+          'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+        },
+      })
+      .then(() => {
+        const idx = replyList.findIndex((item) => item.replyNum === replyNum);
+        const newReplyList = [...replyList];
+        newReplyList.splice(idx, 1);
+        setReplyList(newReplyList);
+        setReplyDeleteFlag(-1);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    return (
-        <div className="reply-delete-modal">
-            <div className="reply-delete-container">
-                <div className="reply-delete-box">
-                    <div>
-                        <button
-                            type="button"
-                            onClick={() => {
-                                replyDeleteHandler(
-                                    replyDeleteFlag,
-                                );
-                            }}
-                        >
-                            삭제
-                        </button>
-                    </div>
-                    <div>
-                        <button
-                            type="button"
-                            onClick={() => {
-                                setReplyDeleteFlag(false);
-                            }}
-                        >
-                            취소
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="reply-delete-modal">
+      <div className="reply-delete-container">
+        <div className="reply-delete-box">
+          <div>
+            <button
+              type="button"
+              onClick={() => {
+                replyDeleteHandler(replyDeleteFlag);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={() => {
+                setReplyDeleteFlag(false);
+              }}
+            >
+              취소
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default DeleteCommentButton;
