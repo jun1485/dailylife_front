9d7b26903fa9dbdb1acec87db1bbd3ce3b91c556
NEARworld <artemismars2@outlook.com>
2022-09-24 21:25:28 +0900

    feat: SingUpInput의 Props 타입 지정

--- src/pages/SignUp/SignUpInput.tsx
+++ src/pages/SignUp/SignUpInput.tsx
@@ -1,5 +1,18 @@
-function SignUpInput({ }) {
-  return <div></div>
+import styled from "styled-components";
+
+interface Props {
+  width?: string;
+  height?: string;
+  text: string;
+  setText: string;
+}
+
+function SignUpInput({ width = '100%', height = '100%', text, setText }: Props) {
+  return <StyledInput></StyledInput>
 }
 
 export default SignUpInput;
+
+const StyledInput = styled.input`
+  
+`
