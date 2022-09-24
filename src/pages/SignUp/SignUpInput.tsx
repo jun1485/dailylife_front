0ab993bbe380e2db3d7f798610176496bda08d01
NEARworld <artemismars2@outlook.com>
2022-09-24 21:56:35 +0900

    chore: input type을 interface에 지정

--- src/pages/SignUp/SignUpInput.tsx
+++ src/pages/SignUp/SignUpInput.tsx
@@ -6,12 +6,13 @@ interface SizeType {
 }
 
 interface Props extends SizeType {
+  type: 'text' | 'email' | 'password';
   text: string;
   setText: string;
 }
 
-function SignUpInput({ width = '100%', height = '55px', text, setText }: Props) {
-  return <StyledInput width={width} height={height}>
+function SignUpInput({ type, width = '100%', height = '55px', text, setText }: Props) {
+  return <StyledInput type={type} width={width} height={height}>
     {text}
   </StyledInput>
 }
