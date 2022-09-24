cc609beadded0153f85bd92dcd91281e2952b5e8
NEARworld <artemismars2@outlook.com>
2022-09-24 21:34:37 +0900

    feat: SignUpInput에 SizeType 지정

--- src/pages/SignUp/SignUpInput.tsx
+++ src/pages/SignUp/SignUpInput.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
-interface Props {
+interface SizeType {
   width?: string;
   height?: string;
+}
+
+interface Props extends SizeType {
   text: string;
   setText: string;
 }
@@ -14,5 +17,5 @@ function SignUpInput({ width = '100%', height = '55px', text, setText }: Props) 
 export default SignUpInput;
 
 const StyledInput = styled.input`
-  
+
 `
